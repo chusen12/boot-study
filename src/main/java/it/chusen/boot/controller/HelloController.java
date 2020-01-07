1ed106d461a32373fd0603d7156d34a942ee6a97
chusen <chusen12@163.com>
2020-01-07 18:03:24 +0800

    last commit

--- src/main/java/it/chusen/boot/controller/HelloController.java
+++ src/main/java/it/chusen/boot/controller/HelloController.java
@@ -16,4 +16,9 @@ public class HelloController {
     public String hello() {
         return "hello world!";
     }
+
+    @GetMapping("/hello2")
+    public String hello2() {
+        return "hello world!";
+    }
 }
