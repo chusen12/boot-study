4b4d5b406f1657371580ee1a384c4bdb4d91322f
chusen <chusen12@163.com>
2020-01-07 17:36:00 +0800

    first commit

---
+++ src/main/java/it/chusen/boot/BootApplication.java
@@ -0,0 +1,16 @@
+package it.chusen.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chusen
+ * @date 2020/1/7 5:07 下午
+ */
+@SpringBootApplication
+public class BootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+}
---
+++ src/main/java/it/chusen/boot/controller/HelloController.java
@@ -0,0 +1,19 @@
+package it.chusen.boot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author chusen
+ * @date 2020/1/7 5:08 下午
+ */
+@RestController
+@RequestMapping("/index")
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "hello world!";
+    }
+}
